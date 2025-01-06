7819edd3150eb6843c1ec42117a0ed8e211a9af9
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-06 22:05:58 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
-import {Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 function App() {
   return (
-    <div>
-      <Button>Logo</Button>
+    <div className="text-center p-4">
+      <div className="first-part">
+        <h1 className="text-stone-700 p-4 text-6xl font-black">
+          Home Page - Introduction to Javascript and Typescript
+        </h1>
+        <Button
+          onClick={() => alert("Hello World thank you for clicking")}
+          className="m-4"
+        >
+          Logo
+        </Button>
+      </div>
+      <div className="second-part">
+        
+      </div>
     </div>
   );
 }
