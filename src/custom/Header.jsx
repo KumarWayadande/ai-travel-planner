e7e7c13949440363b5ebe16de9a2d9c10177cbd4
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-06 23:26:27 +0530

    modified:   src/custom/Header.jsx 	modified:   src/custom/Hero.jsx

--- src/custom/Header.jsx
+++ src/custom/Header.jsx
@@ -1,7 +1,7 @@
 import {Button} from "@/components/ui/button"
 function Header() {
   return (
-    <div className="p-2 shadow-sm">
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
       <img src="../../public/logo.svg" />
       <div>
         <Button>Sign In</Button>
--- src/custom/Hero.jsx
+++ src/custom/Hero.jsx
@@ -1,10 +1,20 @@
-
+import { Button } from "../components/ui/button";
 function Hero() {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center sm:mx-5 md:mx-56 gap-9">
+      <h1 className="font-extrabold text-[50px] text-center mt-16">
+        <span className="text-[#f56551]">
+          Discover your next adventure ti AI :
+        </span>{" "}
+        Personalized ltineraries at Your Fingertips
+      </h1>
+      <p className="text-xl text-gray-500 text-center">
+        Your personalized trip planner and travel curator, creating custom
+        places tailored to your interests and budget.
+      </p>
+      <Button>Get Started, It&apos; Free!</Button>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
