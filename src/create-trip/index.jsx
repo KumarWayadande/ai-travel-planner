21f5dad54b8d3b64899b8a2a686e2704092ea315
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-06 23:44:01 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/create-trip/index.jsx

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,10 +1,19 @@
-
 function CreateTrip() {
   return (
-    <div>
-      CreateTrip
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+      <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
+      <p className="mt-3 text-gray-500 text-xl">
+        Just provide some basic information and our trip planner will generate a
+        customized iltinery based on your preferences.
+      </p>
+
+      <div className="mt-20">
+        <div>
+          <h2 className="text-xl my-3 font-medium">Select Your Destination</h2>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateTrip
+export default CreateTrip;
