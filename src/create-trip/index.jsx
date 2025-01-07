afcacd044de1ffd7b0d678b0fc71795111025450
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-07 11:53:32 +0530

    new file:   src/components/ui/input.jsx 	modified:   src/create-trip/index.jsx     Google Place API added with autocomplete input field

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
-
+import { Input } from "../components/ui/input";
 function CreateTrip() {
+  const [place, setPlace] = useState();
+  console.log(place);
+
   return (
-    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
       <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
       <p className="mt-3 text-gray-500 text-xl">
         Just provide some basic information and our trip planner will generate a
         customized iltinery based on your preferences.
       </p>
 
-      <div className="mt-20">
+      <div className="mt-20 flex flex-col gap-10">
         <div>
           <h2 className="text-xl my-3 font-medium">Select Your Destination</h2>
-          <GooglePlacesAutocomplete 
-          apiKey="AIzaSyA3qR3cksJzDU9sb7aMnJpI4xTT_N58JqI"
+          <GooglePlacesAutocomplete
+            selectProps={{
+              place,
+              onChange: (v) => {
+                setPlace(v);
+                console.log(v);
+              },
+            }}
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
           />
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            How many days for the trip?
+          </h2>
+          <Input type="number" placeholder="Ex.3" />
         </div>
       </div>
     </div>
