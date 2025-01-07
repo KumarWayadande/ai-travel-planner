be5c2b35e483c1fac904147882bba1cbacedd042
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-07 23:14:23 +0530

    modified:   src/view-trip/[tripId]/index.jsx

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -1,6 +1,35 @@
-
+import { useParams } from "react-router-dom";
+import { db } from "../../service/firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import { toast } from "sonner";
+import { useEffect, useState } from "react";
 function Viewtrip() {
-  return <div>ViewTrip</div>;
+  const { tripId } = useParams();
+  const [trip, setTrip] = useState();
+
+  useEffect(() => {
+    tripId && getTripData();
+  }, [tripId]);
+
+  const getTripData = async () => {
+    const docRef = doc(db, "AI-Trip-Information", tripId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      setTrip(docSnap.data());
+    } else {
+      console.log("No such document!");
+      toast("No data found for this trip id");
+    }
+  };
+
+  return <div>
+    {/* Main Information */}
+    {/* Hotel Information */}
+    {/* Itenaries Information */}
+    {/* Footer */}
+  </div>;
 }
 
 export default Viewtrip;
