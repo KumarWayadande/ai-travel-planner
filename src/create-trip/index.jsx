db6d8bc36ac5293a3e56196018d95609b58d8900
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-07 11:20:57 +0530

    modified:   .gitignore 	modified:   README.md 	modified:   src/create-trip/index.jsx     .env.local file added with api key

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,3 +1,5 @@
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+
 function CreateTrip() {
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
@@ -10,6 +12,9 @@ function CreateTrip() {
       <div className="mt-20">
         <div>
           <h2 className="text-xl my-3 font-medium">Select Your Destination</h2>
+          <GooglePlacesAutocomplete 
+          apiKey="AIzaSyA3qR3cksJzDU9sb7aMnJpI4xTT_N58JqI"
+          />
         </div>
       </div>
     </div>
