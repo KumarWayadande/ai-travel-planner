29f3ba71c820c9678be42faa64abd03bd9006f19
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-08 17:06:09 +0530

    modified:   src/view-trip/components/PlacesToVisit.jsx     places fetched and displayed on the screen successfully

--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -4,10 +4,10 @@ function PlacesToVisit({ trip }) {
     <div>
       <h2 className="font-bold text-lg">Place to visit</h2>
       <div>
-        {trip.fetchedTripData.itinerary.map((item, index) => {
+        {trip?.fetchedTripData?.itinerary.map((item, index) => {
           return (
             <div key={index}>
-              <h2 className="font-medium text-lg">{item.day}</h2>
+              <h2 className="font-medium text-lg">Day {item.day}</h2>
               {item.plan.map((place, indexInner) => {
                 return (
                   <div key={`${index}${indexInner}`}>
