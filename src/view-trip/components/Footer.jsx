66bd3772bfed5b3d4171f465623e534b0df19ae5
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-08 18:01:36 +0530

    new file:   src/view-trip/components/Footer.jsx

---
+++ src/view-trip/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="pt-10 mt-20 border-t-2 text-center">
+      <h2 className="text-gray-600"> Created by AI Trip Planner Designed with ❤️ by Kumar Wayadande</h2>
+    </div>
+  );
+}
+
+export default Footer;
