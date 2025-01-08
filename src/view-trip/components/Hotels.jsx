b96ccf52a0f7d05d96c149f8f952a1cb0820ec63
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-08 16:47:22 +0530

    modified:   src/view-trip/components/Hotels.jsx

--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -1,28 +1,36 @@
 /* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom";
 function Hotels({ trip }) {
   return (
     <div>
-      <h2 className="font-bold text-xl mt-5">Hotels Recommendation</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      <h2 className="font-bold text-xl my-5">Hotels Recommendation</h2>
+      <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {trip?.fetchedTripData?.hotelOptions.map((hotel, index) => {
           return (
-            <div key={index}>
-              <img
-                className="rounded-xl"
-                // src={hotel.hotelImageUrl}
-                src="../../../public/road-trip-vacation.jpg"
-                alt=""
-              />
+            <Link
+              key={index}
+              target="_blank"
+              to={`https://www.google.com/maps/search/?api=1&query=${hotel.hotelName} ${hotel.hotelAddress}`}
+            >
+              <div className="cursor-pointer hover:scale-110 transition-all">
+                <img
+                  className="rounded-xl"
+                  // src={hotel.hotelImageUrl}
+                  src="../../../public/road-trip-vacation.jpg"
+                  alt=""
+                />
 
-              <div className="my-2">
-                <h2 className="font-medium ">{hotel.hotelName}</h2>
-                <h2 className="text-xs text-gray-500">
-                  📍{hotel.hotelAddress}
-                </h2>
-                <h2 className="text-sm">💰{hotel.price}</h2>
-                <h2 className="text-sm">⭐{hotel.rating}</h2>
+                <div className="my-2">
+                  <h2 className="font-medium ">{hotel.hotelName}</h2>
+                  <h2 className="text-xs text-gray-500">
+                    📍{hotel.hotelAddress}
+                  </h2>
+                  <h2 className="text-sm">💰{hotel.price}</h2>
+                  <h2 className="text-sm">⭐{hotel.rating}</h2>
+                </div>
               </div>
-            </div>
+            </Link>
           );
         })}
       </div>
