921045b8dee4870fc20cfebd50e11322f315f7b2
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-08 18:12:32 +0530

    modified:   src/view-trip/components/Footer.jsx

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="pt-10 mt-20 border-t-2 text-center">
-      <h2 className="text-gray-600"> Created by AI Trip Planner Designed with ❤️ by Kumar Wayadande</h2>
+      <h2 className="text-gray-500"> Created by AI Trip Planner Designed with ❤️ by Kumar Wayadande</h2>
     </div>
   );
 }
