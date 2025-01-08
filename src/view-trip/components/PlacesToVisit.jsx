bd05c84f373e23778f648ea54af05f1d981a9927
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-08 18:01:54 +0530

    modified:   src/view-trip/components/PlacesToVisit.jsx

--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -1,20 +1,27 @@
+import PlaceCardItem from "./PlaceCardItem";
+
 /* eslint-disable react/prop-types */
 function PlacesToVisit({ trip }) {
   return (
     <div>
-      <h2 className="font-bold text-lg">Place to visit</h2>
+      <h2 className="font-bold text-lg my-10">Place to visit</h2>
       <div>
         {trip?.fetchedTripData?.itinerary.map((item, index) => {
           return (
             <div key={index}>
-              <h2 className="font-medium text-lg">Day {item.day}</h2>
-              {item.plan.map((place, indexInner) => {
-                return (
-                  <div key={`${index}${indexInner}`}>
-                    <h2>{place.placeName}</h2>
-                  </div>
-                );
-              })}
+              <h2 className="font-medium text-lg mt-10">Day {item.day}</h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                {item.plan.map((place, indexInner) => {
+                  return (
+                    <div key={`${index}${indexInner}`} className="my-3">
+                      <h2 className="font-medium text-sm text-orange-400 my-2">
+                        {place.time}
+                      </h2>
+                      <PlaceCardItem place={place} />
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           );
         })}
