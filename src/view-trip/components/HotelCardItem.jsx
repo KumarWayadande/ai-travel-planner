2985d0a8462292ac1aa7bc017719541a38d878a3
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-09 11:26:09 +0530

    modified:   src/view-trip/components/HotelCardItem.jsx

--- src/view-trip/components/HotelCardItem.jsx
+++ src/view-trip/components/HotelCardItem.jsx
@@ -1,10 +1,28 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 
-function HotelCardItem({hotelData}) {
+function HotelCardItem({ hotel }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Link
+      target="_blank"
+      to={`https://www.google.com/maps/search/?api=1&query=${hotel.hotelName} ${hotel.hotelAddress}`}
+    >
+      <div className="cursor-pointer hover:scale-110 transition-all">
+        <img
+          className="rounded-xl"
+          // src={hotel.hotelImageUrl}
+          src="../../../public/road-trip-vacation.jpg"
+        />
+
+        <div className="my-2">
+          <h2 className="font-medium ">{hotel.hotelName}</h2>
+          <h2 className="text-xs text-gray-500">📍{hotel.hotelAddress}</h2>
+          <h2 className="text-sm">💰{hotel.price}</h2>
+          <h2 className="text-sm">⭐{hotel.rating}</h2>
+        </div>
+      </div>
+    </Link>
+);
 }
 
-export default HotelCardItem
+export default HotelCardItem;
