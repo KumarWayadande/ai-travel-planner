520771b1f199d7e6f9b04ac9c057a4da4a61d141
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-09 12:43:20 +0530

    modified:   src/custom/Header.jsx

--- src/custom/Header.jsx
+++ src/custom/Header.jsx
@@ -1,10 +1,32 @@
-import {Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 function Header() {
+  const userData = localStorage.getItem("user");
+  console.log(JSON.parse(userData));
+
+  const handleSignUp = () => {
+    alert("e");
+  };
+  const handleSignIn = () => {
+    alert("e");
+  };
+
   return (
     <div className="p-3 shadow-sm flex justify-between items-center px-5">
       <img src="../../public/logo.svg" />
       <div>
-        <Button>Sign In</Button>
+        {!userData && <Button onClick={handleSignIn}>Sign In</Button>}
+        {userData && (
+          <div>
+            <Button
+              variant="outline"
+              className="rounded-full"
+              onClick={handleSignUp}
+            >
+              My Trips
+            </Button>
+            <img src={userData.picture} alt="" />
+          </div>
+        )}
       </div>
     </div>
   );
